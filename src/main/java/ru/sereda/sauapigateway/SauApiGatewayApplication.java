8d9dbdfb64db8c51d98806bd1ad494f25942c1dc
Михаил <mikhsereda@gmail.com>
2023-03-10 01:14:24 +1000

    Initial commit

---
+++ src/main/java/ru/sereda/sauapigateway/SauApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.sereda.sauapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SauApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sereda/sauapigateway/SauApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sereda.sauapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
